4df73fb5496882737a4c9c7a1f5dddfa255d909b
DESKTOP-QO3JRCN\Admin <hoanqhd@gmail.com>
2018-02-07 10:11:04 +0700

    Update code

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -35,7 +35,7 @@ do_action( 'woocommerce_before_main_content' );
 
 ?>
 <header class="woocommerce-products-header">
-	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) && $sh_option['display-pagetitlebar'] == '0' ) ) : ?>
+	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) && $sh_option['display-pagetitlebar'] == '0' ) : ?>
 		<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
 	<?php endif; ?>
 
